c0da41b384278436de8953beb5b8f449e6e4eece
ITE03051082 <hower@hower-VirtualBox.(none)>
2016-09-19 16:49:21 +0800

    9/19

---
+++ tests.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("請輸入十個數字, 用空格隔開");
+	int a1,a2,a3,a4,a5,a6,a7,a8,a9,a0;
+	scanf("%d %d %d %d %d %d %d %d %d %d",&a1,&a2,&a3,&a4,&a5,&a6,&a7,&a8,&a9,&a0);
+	printf("您輸入的數字為：\n");
+	printf("%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n",a1,a2,a3,a4,a5,a6,a7,a8,a9,a0);
+}
